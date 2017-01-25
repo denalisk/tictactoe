5f56870c1a3abdb21f1c8b029d0f8f3be0ca2e81
Nicole Sanders and Sam Kirsch <sam@>
2017-01-25 09:46:59 -0800

    call the switchState function and switch between x and o

--- js/scripts.js
+++ js/scripts.js
@@ -15,10 +15,10 @@ function Player(playerName, symbol, game) {
 }
 
 Game.prototype.stateSwitch = function() {
-  if (playerState === 0) {
-    playerState = 1;
+  if (this.playerState === 0) {
+    this.playerState = 1;
   } else {
-    playerState = 0;
+    this.playerState = 0;
   }
 }
 
@@ -45,9 +45,18 @@ Game.prototype.printBoard = function(outputLocation) {
 
 $(document).ready(function() {
   var player1 = new Player("Sam", "X");
-  var player2 = new Player("Nicole", "X");
+  var player2 = new Player("Nicole", "O");
   var game1 = new Game(playerArray);
 
   game1.printBoard("#board");
+
+  $(".move-button").click(function() {
+    var vectorIndex = parseInt($(this).val());
+    console.log(vectorIndex);
+    game1.valueVector[vectorIndex] = game1.playerArray[game1.playerState].symbol;
+    $("#board").empty();
+    game1.printBoard("#board");
+    game1.stateSwitch();
+  })
 
 })
