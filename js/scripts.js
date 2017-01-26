8f7d70579b5b70ce089e7143e324d156c7801db9
Nicole Sanders and Sam Kirsch <nicole@>
2017-01-26 10:37:05 -0800

    add functions and make minor refactors to allow the computer to move randomly

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,39 @@
 var playerArray = [];
 var currentGame;
+var onePlayer = false;
 
-function Game(playerArray) {
+function Game(player1, player2, onePlayer, computerPlayer) {
   this.valueVector = [0,0,0,0,0,0,0,0,0];
   this.gameState = true;
   this.playerState = 0;
-  this.playerArray = playerArray;
+  this.playerArray = [];
+  this.playerArray.push(player1, player2, computerPlayer);
   this.currentPlayer = playerArray[this.playerState]
   this.winner = "It's a tie";
-  this.onePlayer = true;
+  this.onePlayer = onePlayer;
 }
 
-function Player(playerName) {
+function Player(playerName, identifier) {
   this.playerName = playerName;
-  this.symbol = "X";
+  this.identifier = identifier;
   playerArray.push(this);
   this.winsTotal = 0;
   this.humanPlayer = true;
 }
 
-var makeComputerPlayer = function() {
-  var computer = new Player("Computer");
-  this.humanPlayer = false;
-}
+Game.prototype.randomNumber = function() {
+  var computerMove = Math.floor(Math.random()*9);
+  while (this.valueVector[computerMove] != 0) {
+    computerMove = Math.floor(Math.random()*9)
+  }
+  return computerMove;
+};
 
 Game.prototype.onePlayerSetup = function() {
-  if (this.playerArray[0].humanPlayer === false) {
-    this.onePlayer = true;
+  if (this.onePlayer === true) {
+    this.playerArray.splice(1,1);
+  } else {
     this.playerArray.pop();
-    this.playerArray.reverse();
   }
 }
 
@@ -62,26 +67,30 @@ Game.prototype.printBoard = function(outputLocation) {
 
 Game.prototype.checkWin = function() {
   var firstElement = this.valueVector[0];
+  var winIndex;
   var winner = false;
   var completeGame = true;
   for (var index = 0; index < 7; index += 3) {
     if (this.valueVector[index] === this.valueVector[index + 1] && this.valueVector[index] === this.valueVector[index + 2] && this.valueVector[index] != 0) {
       winner = true;
+      winIndex = this.valueVector[index];
     }
   }
   for (var index = 0; index < 3; index += 1) {
     if (this.valueVector[index] === this.valueVector[index + 3] && this.valueVector[index] === this.valueVector[index + 6] && this.valueVector[index] != 0) {
       winner = true;
+      winIndex = this.valueVector[index];
     }
   }
   for (var index = 2; index < 5; index += 2) {
     if (this.valueVector[4] === this.valueVector[4 + index] && this.valueVector[4] === this.valueVector[4 - index] && this.valueVector[4] != 0) {
       winner = true;
+      winIndex = this.valueVector[index];
     }
   }
   if (winner === true) {
-    this.winner = this.playerArray[this.playerState].playerName + " " + "is the winner!";
-    this.playerArray[this.playerState].winsTotal += 1;
+    this.winner = this.playerArray[winIndex-1].playerName + " " + "is the winner!";
+    this.playerArray[winIndex-1].winsTotal += 1;
   }
   return winner;
 }
@@ -132,7 +141,7 @@ $(document).ready(function() {
     $("#name-players").show();
     if (playerChoice === "one-player") {
       $(".player2-hidden").hide();
-      makeComputerPlayer();
+      onePlayer = true;
     }
   })
 
@@ -153,9 +162,11 @@ $(document).ready(function() {
 
     $(".entire-game").show();
     $(".landing-page").hide();
-    var player1 = new Player($("#player-1").val());
-    var player2 = new Player($("#player-2").val());
-    var game1 = new Game(playerArray);
+    var computerPlayer = new Player("Computer", 2);
+    computerPlayer.humanPlayer = false;
+    var player1 = new Player($("#player-1").val(), 1);
+    var player2 = new Player($("#player-2").val(), 2);
+    var game1 = new Game(player1, player2, onePlayer, computerPlayer);
     game1.onePlayerSetup();
     currentGame = game1;
 
@@ -163,13 +174,21 @@ $(document).ready(function() {
       var currentDiv = $(this);
       var vectorIndex = parseInt(currentDiv.attr('id'));
       if (currentGame.valueVector[vectorIndex] === 0 && currentGame.gameState === true) {
-        if (currentGame.playerState === 0) {
-          var imageId = "#x";
+        if (currentGame.playerState === 0 && currentGame.onePlayer === true) {
+          currentGame.imageInsert("#x", $(this));
+          currentGame.valueVector[vectorIndex] = currentGame.playerState+1;
+          var computerMove = currentGame.randomNumber();
+          currentGame.imageInsert("#o", "#" + computerMove);
+          currentGame.valueVector[computerMove] = 2;
+        } else if (currentGame.playerState === 0) {
+          currentGame.imageInsert("#x", $(this));
+          currentGame.valueVector[vectorIndex] = currentGame.playerState+1;
+          currentGame.stateSwitch();
         } else {
           var imageId = "#o";
+          currentGame.valueVector[vectorIndex] = currentGame.playerState+1;
+          currentGame.stateSwitch();
         }
-        currentGame.imageInsert(imageId, currentDiv);
-        currentGame.valueVector[vectorIndex] = currentGame.playerState+1;
         if (currentGame.checkOver() === true){
           $("#game-over-text").text(currentGame.winner);
           currentGame.gameState = false;
@@ -179,12 +198,12 @@ $(document).ready(function() {
           $("#player-2-name").text(currentGame.playerArray[1].playerName);
           $("#player-2-wins").text(currentGame.playerArray[1].winsTotal);
         }
-        currentGame.stateSwitch();
       }
     })
 
     $(".replay").click(function(){
-      var newGame = new Game(playerArray);
+      var newGame = new Game(player1, player2, onePlayer, computerPlayer);
+      newGame.onePlayerSetup();
       currentGame = newGame;
       $("#game-over").hide();
       $(".entire-game").find(".position").empty();
